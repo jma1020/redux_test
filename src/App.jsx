99d3e5f8cff30604e424edb87dcf0509289964ba
정민킴 <jma1020@naver.com>
2022-05-25 23:24:08 +0900

    :hammer:   eslint 및 prettier 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("sadas");
+  const rrs = 3;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit {rrs}
+          <code>src/App.js</code>
+          and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +28,7 @@ function App() {
     </div>
   );
 }
+
+/// 이거는 npx eslint --init 으로 생성
 
 export default App;
