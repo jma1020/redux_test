32ab74310bfbc04565288956af431efbf6b13ebb
정민킴 <jma1020@naver.com>
2022-06-08 00:53:19 +0900

    ⚡️ 프레젠테이션 Todo 컴포넌트 생성

---
+++ src/components/Todos.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+// 컴포넌트 최적화를 위하여 React.memo 사용
+const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
+  return (
+    <li
+      style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+});
+
+const TodoList = React.memo(function TodoList({ todos, onToggle }) {
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} onToggle={onToggle} />;
+      })}
+    </ul>
+  );
+});
+
+function Todos({ todos, onCreate, onToggle }) {
+  // 리덕스를 사용한다고 해서 모든 상태를 리덕스에서 관리해야하는 것은 아닙니다.
+  const [text, setText] = useState('');
+  const onChange = (e) => setText(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onCreate(text);
+    setText('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={text} placeholder="입력하세요" onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      <TodoList todos={todos} onToggle={onToggle} />
+    </div>
+  );
+}
+
+export default Todos;
