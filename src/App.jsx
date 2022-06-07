d2e93798098404b8391fc6b45b8e46a2133c6faa
정민킴 <jma1020@naver.com>
2022-06-08 00:52:57 +0900

    ⚡️컨테이너 컴포넌트 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 function App() {
   const rrs = 3;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit {rrs}
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
     </div>
   );
 }
